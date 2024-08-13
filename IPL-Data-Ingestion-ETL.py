b505f10307844a75b4f56f69965b8ddd1080e59c
somnathkhanra1998@gmail.com <somnathkhanra1998@gmail.com>
2024-08-13 16:30:49 +0000

    Trying to mount Azure DataLake file system to DBFS

---
+++ IPL-Data-Ingestion-ETL.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+
+from pyspark.sql.functions import col
+from pyspark.sql.types import IntegerType, DoubleType, BooleanType, DateType
+
+# COMMAND ----------
+
+
+dbutils.fs.mounts()
+
+# COMMAND ----------
+
+
